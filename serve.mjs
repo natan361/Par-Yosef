47cc9fb751d713fefeefcec45c0a523ddbaf9acf
nataniger <ntyyml72@gmail.com>
2026-07-22 14:00:25 +0300

    לוגו אימות

--- serve.mjs
+++ serve.mjs
@@ -16,6 +16,8 @@ const MIME = {
   '.js':   'application/javascript; charset=utf-8',
   '.mjs':  'application/javascript; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
+  '.webmanifest': 'application/manifest+json; charset=utf-8',
+  '.xml':  'application/xml; charset=utf-8',
   '.png':  'image/png',
   '.jpg':  'image/jpeg',
   '.jpeg': 'image/jpeg',
